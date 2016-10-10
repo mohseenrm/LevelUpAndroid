6f85e4cb683248885e03d50cdde8a98556b152f1
mshekh05 <mohdshekh05@gmail.com>
2016-10-10 13:06:40 -0700

    login and signup page

---
+++ app/src/main/java/com/example/mohseenmukaddam/levelup/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.example.mohseenmukaddam.levelup;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Mohd on 10/9/2016.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
---
+++ app/src/main/java/com/example/mohseenmukaddam/levelup/SignupActivity.java
@@ -0,0 +1,9 @@
+package com.example.mohseenmukaddam.levelup;
+
+import android.app.Activity;
+
+/**
+ * Created by Mohd on 10/10/2016.
+ */
+public class SignupActivity extends Activity {
+}
