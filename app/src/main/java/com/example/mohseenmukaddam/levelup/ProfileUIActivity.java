3648e105eb9f739593df77d0f15f1f3c21091516
Hitendra Shukla <hitendrashukla111@gmail.com>
2016-10-27 08:15:02 -0700

    Profile UI changes

--- app/src/main/java/com/example/mohseenmukaddam/levelup/ProfileUIActivity.java
+++ app/src/main/java/com/example/mohseenmukaddam/levelup/ProfileUIActivity.java
@@ -2,14 +2,21 @@ package com.example.mohseenmukaddam.levelup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+import com.github.mikephil.charting.charts.PieChart;
+
 /*
  *
  */
 public class ProfileUIActivity extends AppCompatActivity {
 
+    private RelativeLayout profileDetails;
+    private PieChart mChart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_ui);
+        profileDetails = (RelativeLayout) findViewById(R.id.ProfileDetails);
+
     }
 }
