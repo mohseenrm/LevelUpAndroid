5609feef546cbfe686b3b947d66c1785a1ca08e0
Hitendra Shukla <hitendrashukla111@gmail.com>
2016-10-27 01:59:39 -0700

    WIP layout

---
+++ app/src/main/java/com/example/mohseenmukaddam/levelup/ProfileUIActivity.java
@@ -0,0 +1,15 @@
+package com.example.mohseenmukaddam.levelup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/*
+ *
+ */
+public class ProfileUIActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_ui);
+    }
+}
