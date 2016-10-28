0d453a030c4b6e4d1cdb39eae3124b5dafeec475
mohseenrm <mohseenmukaddam6@gmail.com>
2016-10-28 14:27:19 -0700

    adding sprites

--- app/src/main/java/com/example/mohseenmukaddam/levelup/TestLayout.java
+++ app/src/main/java/com/example/mohseenmukaddam/levelup/TestLayout.java
@@ -4,14 +4,23 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.mohseenmukaddam.levelup.animation.CharacterAnimated;
+
 /**
  * Created by mohseenmukaddam on 10/27/16.
  */
 
 public class TestLayout extends AppCompatActivity {
+    CharacterAnimated L;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_ui);
+
+        //this.L = new CharacterAnimated( )
+    }
+
+    public void testUpdate(){
+
     }
 }
