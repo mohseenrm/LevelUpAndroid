1b473bfa93fac62b52f9d0048430b3f864d3afbf
mohseenrm <mohseenmukaddam6@gmail.com>
2016-10-27 18:42:27 -0700

    adding links for graphs

--- app/src/main/java/com/example/mohseenmukaddam/levelup/baseclasses/Constants.java
+++ app/src/main/java/com/example/mohseenmukaddam/levelup/baseclasses/Constants.java
@@ -8,6 +8,13 @@ package com.example.mohseenmukaddam.levelup.baseclasses;
 //https://android-arsenal.com/details/1/3186
 //https://android-arsenal.com/details/1/2961
 //https://android-arsenal.com/details/1/2897
+
+/**
+ * Radar graph links:
+ * https://android-arsenal.com/details/1/4528
+ * https://android-arsenal.com/details/1/4533
+ * https://github.com/PhilJay/MPAndroidChart
+ */
 public class Constants {
     public enum LearningRate{
         SLOW, NORMAL, FAST
