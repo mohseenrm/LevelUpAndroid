e03feb8cd89c933e0f70ea0e9fe17b18ffcbb63c
mohseenrm <mohseenmukaddam6@gmail.com>
2016-10-28 20:45:22 -0700

    adding tags

--- app/src/main/java/com/example/mohseenmukaddam/levelup/TestLayout.java
+++ app/src/main/java/com/example/mohseenmukaddam/levelup/TestLayout.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import org.honorato.multistatetogglebutton.MultiStateToggleButton;
+
 /**
  * Created by mohseenmukaddam on 10/27/16.
  */
@@ -12,7 +14,13 @@ public class TestLayout extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile_ui);
+        setContentView(R.layout.task_builder);
+
+        MultiStateToggleButton skillButtons1 = ( MultiStateToggleButton ) this.findViewById( R.id.mstb_multi_id_1 );
+        MultiStateToggleButton skillButtons2 = ( MultiStateToggleButton ) this.findViewById( R.id.mstb_multi_id_2 );
+
+        skillButtons1.enableMultipleChoice( true );
+        skillButtons2.enableMultipleChoice( true );
 
         //this.L = new CharacterAnimated( )
     }
