70233405a8a4dddce6f55ce721ad126c1a670066
mohseenrm <mohseenmukaddam6@gmail.com>
2016-10-30 16:44:22 -0700

    adding butterknife

--- app/src/main/java/com/example/mohseenmukaddam/levelup/TestLayout.java
+++ app/src/main/java/com/example/mohseenmukaddam/levelup/TestLayout.java
@@ -6,18 +6,20 @@ import android.support.v7.app.AppCompatActivity;
 
 import org.honorato.multistatetogglebutton.MultiStateToggleButton;
 
+import butterknife.BindView;
+
 /**
  * Created by mohseenmukaddam on 10/27/16.
  */
 
 public class TestLayout extends AppCompatActivity {
+    @BindView( R.id.mstb_multi_id_1 ) MultiStateToggleButton skillButtons1;
+    @BindView( R.id.mstb_multi_id_2 ) MultiStateToggleButton skillButtons2;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.task_builder);
-
-        MultiStateToggleButton skillButtons1 = ( MultiStateToggleButton ) this.findViewById( R.id.mstb_multi_id_1 );
-        MultiStateToggleButton skillButtons2 = ( MultiStateToggleButton ) this.findViewById( R.id.mstb_multi_id_2 );
 
         skillButtons1.enableMultipleChoice( true );
         skillButtons2.enableMultipleChoice( true );
