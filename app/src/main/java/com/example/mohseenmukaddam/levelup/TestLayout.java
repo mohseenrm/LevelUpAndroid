fb660d3ba8410e13940fb9958a3fef294f3765dd
mshekh05 <mohdshekh05@gmail.com>
2016-11-12 15:34:20 -0700

    m changes

--- app/src/main/java/com/example/mohseenmukaddam/levelup/TestLayout.java
+++ app/src/main/java/com/example/mohseenmukaddam/levelup/TestLayout.java
@@ -3,11 +3,8 @@ package com.example.mohseenmukaddam.levelup;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import org.androidannotations.annotations.AfterViews;
-import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 import org.honorato.multistatetogglebutton.MultiStateToggleButton;
@@ -19,22 +16,25 @@ import org.honorato.multistatetogglebutton.MultiStateToggleButton;
  */
 @EActivity( R.layout.task_builder )
 public class TestLayout extends AppCompatActivity {
+
     @ViewById( R.id.mstb_multi_id_1 )
     MultiStateToggleButton skillButtons1;
 
     @ViewById( R.id.mstb_multi_id_2 )
     MultiStateToggleButton skillButtons2;
 
-    @ViewById
-    EditText task_input;
-
-    @ViewById
-    EditText task_description;
+    //@BindView( R.id.mstb_multi_id_1 ) MultiStateToggleButton skillButtons1;
+    //@BindView( R.id.mstb_multi_id_2 ) MultiStateToggleButton skillButtons2;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         //setContentView(R.layout.task_builder);
+        // setContentView(R.layout.user_creation);
+        //added butterknife
+        //skillButtons1.enableMultipleChoice( true );
+        //skillButtons2.enableMultipleChoice( true );
 
 
         //this.L = new CharacterAnimated( )
@@ -44,19 +44,5 @@ public class TestLayout extends AppCompatActivity {
         //added android annotations
         skillButtons1.enableMultipleChoice( true );
         skillButtons2.enableMultipleChoice( true );
-    }
-    @Click( R.id.confirm_button )
-    void validate_and_confirm(){
-        final String task_name = task_input.getText().toString();
-        final String task_desc = task_description.getText().toString();
-
-        if( task_desc != null && task_name != null ){
-            //TODO: load profile page
-            //TODO: add record to database
-            setContentView( R.layout.activity_profile_ui );
-        }
-        else
-            Toast.makeText( this, "Enter some valid name and description", Toast.LENGTH_SHORT ).show();
-
     }
 }
