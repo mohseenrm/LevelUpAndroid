623cd27e60907a31124ecca8628934ade920e724
mohseenrm <mohseenmukaddam6@gmail.com>
2016-11-12 15:48:28 -0700

    adding changes from mami

--- app/src/main/java/com/example/mohseenmukaddam/levelup/TestLayout.java
+++ app/src/main/java/com/example/mohseenmukaddam/levelup/TestLayout.java
@@ -3,8 +3,12 @@ package com.example.mohseenmukaddam.levelup;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 import org.honorato.multistatetogglebutton.MultiStateToggleButton;
@@ -14,7 +18,7 @@ import org.honorato.multistatetogglebutton.MultiStateToggleButton;
 /**
  * Created by mohseenmukaddam on 10/27/16.
  */
-@EActivity( R.layout.task_builder )
+@EActivity( R.layout.task_builder_2 )
 public class TestLayout extends AppCompatActivity {
 
     @ViewById( R.id.mstb_multi_id_1 )
@@ -22,6 +26,12 @@ public class TestLayout extends AppCompatActivity {
 
     @ViewById( R.id.mstb_multi_id_2 )
     MultiStateToggleButton skillButtons2;
+
+    @ViewById
+    EditText task_name;
+
+    @ViewById
+    EditText task_description;
 
     //@BindView( R.id.mstb_multi_id_1 ) MultiStateToggleButton skillButtons1;
     //@BindView( R.id.mstb_multi_id_2 ) MultiStateToggleButton skillButtons2;
@@ -45,4 +55,17 @@ public class TestLayout extends AppCompatActivity {
         skillButtons1.enableMultipleChoice( true );
         skillButtons2.enableMultipleChoice( true );
     }
+
+    @Click( R.id.button_confirm )
+    void validate_and_confirm(){
+        final String task_name_str = task_name.getText().toString();
+        final String task_desc = task_description.getText().toString();
+        if( task_desc != null && task_name_str != null ){
+            //TODO: load profile page
+            // TODO: add record to database/
+            setContentView( R.layout.activity_profile_ui );
+        }
+        else
+            Toast.makeText( this, "Enter some valid name and description", Toast.LENGTH_SHORT ).show();
+        }
 }
