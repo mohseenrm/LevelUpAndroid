4475c32ad7de36804d7573e78a7994acec7c7914
Hitendra Shukla <hitendrashukla111@gmail.com>
2016-11-24 11:14:08 -0700

    UserSettings Done

--- app/src/main/java/com/example/mohseenmukaddam/levelup/UserSettings.java
+++ app/src/main/java/com/example/mohseenmukaddam/levelup/UserSettings.java
@@ -25,32 +25,67 @@ import info.hoang8f.widget.FButton;
 public class UserSettings extends AppCompatActivity {
     private final String TAG = "SETTINGS ACTIVITY";
     private ImageView clickedImageView = null;
+    private String avatarName = "";
     EditText editText ;
+    List<ImageView> imageViews;
 
-    public void initVar(){
-        editText = (EditText) findViewById(R.id.editTextAvName);
-        List<ImageView> imageViews = new ArrayList<>(12);
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_creation);
+        init();
+    }
+
+    public void resetRemainingImageViews(int id){
+        for(int i=0; i<imageViews.size(); i++){
+            ImageView tmp;
+            tmp = imageViews.get(i);
+            if(tmp.getId() != id){
+                tmp.setBackgroundResource(R.drawable.button_not_pressed);
+            }
+        }
+    }
+    public void init(){
         FButton fButton;
+        editText = (EditText) findViewById(R.id.editTextAvName);
+        imageViews = new ArrayList<>(12);
+        clickedImageView = (ImageView) findViewById(R.id.imageView1);
 
+        // Populate ImageViews
+        for(int i=1; i<=12; i++){
+
+            imageViews.add((ImageView) findViewById(R.id.imageView1));
+            imageViews.add((ImageView) findViewById(R.id.imageView2));
+            imageViews.add((ImageView) findViewById(R.id.imageView3));
+            imageViews.add((ImageView) findViewById(R.id.imageView4));
+            imageViews.add((ImageView) findViewById(R.id.imageView5));
+            imageViews.add((ImageView) findViewById(R.id.imageView6));
+            imageViews.add((ImageView) findViewById(R.id.imageView7));
+            imageViews.add((ImageView) findViewById(R.id.imageView8));
+            imageViews.add((ImageView) findViewById(R.id.imageView9));
+            imageViews.add((ImageView) findViewById(R.id.imageView10));
+            imageViews.add((ImageView) findViewById(R.id.imageView11));
+            imageViews.add((ImageView) findViewById(R.id.imageView12));
+        }
+
+        // Registering ImageViews Listeners
         for (int i=0; i<imageViews.size(); i++){
             ImageView currentImgView = (ImageView) imageViews.get(i);
             currentImgView.setClickable(true);
             currentImgView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-//                    Toast.makeText(YourActivityName.this,
-//                            "The favorite list would appear on clicking this icon",
-//                            Toast.LENGTH_LONG).show();
                     clickedImageView = (ImageView) v;
-                    //clickedImageView.setColorFilter(Color.BLUE);
-                    //Log.d(TAG,clickedImageView.get);
                     Log.d(TAG,"Image CLicked: "+clickedImageView.getId());
 
-                    //clickedImageView.setBackgroundResource(R.drawable.button_pressed);
+                    clickedImageView.setBackgroundResource(R.drawable.button_pressed);
+                    resetRemainingImageViews(clickedImageView.getId());
                 }
             });
         }
 
+        // Registering Confirm Button Listeners
         fButton = (FButton) findViewById(R.id.button4);
         fButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,19 +97,19 @@ public class UserSettings extends AppCompatActivity {
 
 
     public void doWorkAfterComfim(){
-        Log.d(TAG,"Button CLicked: Doing Work ...");
 
         if (editText.getText().length() != 0){
-            Log.d(TAG,"Doing Work ..."+ editText.getText());
+            avatarName = editText.getText().toString();
+            //Log.d(TAG,"Doing Work ..."+ editText.getText());
         }else {
-            Log.d(TAG,"Done TextView");
+            avatarName = "Player1";
+            //Log.d(TAG,"Done TextView");
         }
+
+        // TODO: 11/24/2016 ---> Add here whatever Confirm Button should do
+        // You have : clickedImageView amd avatarName to play with
+
     }
 
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.user_creation);
-        initVar();
-    }
+
 }
