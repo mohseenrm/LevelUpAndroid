cccf3a8a6d559b0747226c77fe645e863c4da3b6
Santosh Bidve <santoshbidave2@gmail.com>
2016-11-24 04:53:36 -0700

    splash screen

---
+++ app/src/main/java/com/example/mohseenmukaddam/levelup/SplashScreenNew.java
@@ -0,0 +1,49 @@
+package com.example.mohseenmukaddam.levelup;
+
+import android.animation.ObjectAnimator;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+/**
+ * Created by Santosh on 11/24/2016.
+ */
+
+public class SplashScreenNew extends AppCompatActivity {
+
+    ProgressBar mprogressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+        Animation anim1 = AnimationUtils.loadAnimation(this,R.anim.anim_down);
+        ImageView img =(ImageView)findViewById(R.id.imageView);
+        img.setAnimation(anim1);
+
+        mprogressBar = (ProgressBar) findViewById(R.id.progressBar);
+        ObjectAnimator anim = ObjectAnimator.ofInt(mprogressBar, "progress", 0, 100);
+        anim.setDuration(4000);
+        anim.setInterpolator(new DecelerateInterpolator());
+        anim.start();
+
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(new Intent(SplashScreenNew.this,MainActivity.class));
+                finish();
+
+            }
+        },3000);
+    }
+}
